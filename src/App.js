560b7bfbafa93b02327849b08140601b1d906a33
kalkanozgur <ahmetozgurkalkan@gmail.com>
2022-09-29 16:13:37 +0300

    addContact

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
+import Contacts from "./components/Contacts";
+
 function App() {
-	return <div></div>;
+	return (
+		<div className="flex flex-col items-center">
+			<h1 className="text-3xl">Contacts</h1>
+			<Contacts />
+		</div>
+	);
 }
 
 export default App;
---
+++ src/components/Contacts/Form.js
@@ -0,0 +1,32 @@
+import React, { useState } from "react";
+
+import { useDispatch } from "react-redux";
+import { nanoid } from "@reduxjs/toolkit";
+import { addContact } from "./../../context/contactSlice";
+
+function Form() {
+	const [name, setName] = useState("");
+	const dispatch = useDispatch();
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		if (!name) {
+			return false;
+		}
+		dispatch(addContact({ id: nanoid(), name }));
+		setName("");
+	};
+	return (
+		<div>
+			<form onSubmit={handleSubmit}>
+				<input
+					type="text"
+					value={name}
+					onChange={(e) => setName(e.target.value)}
+					placeholder={"Name"}
+				/>
+			</form>
+		</div>
+	);
+}
+
+export default Form;
